1d974d60900f06f19b3a72d250880e2ddce1f950
infernalslam <infernal-slam@hotmail.com>
2018-08-22 14:56:19 +0700

    fix code api

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
+const express = require('express')
+const app = express()
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
 const dbName = 'local'
-
-function getValueForNextSequence(sequenceOfName, db, num){
-   var sequenceDoc = db.findAndModify({
-      query:{_id: sequenceOfName },
-      update: {$inc:{count: num}},
-      new:true
-   })
-   return sequenceDoc.sequence_value
-}
-
-let db
-MongoClient.connect(url, function(err, client) {
-  db = client.db(dbName)
-})
-
-
-const express = require('express')
-const app = express()
+let db = null
+MongoClient.connect(url, function(err, client) { db = client.db(dbName) })
 
 app.get('/', (req, res) => {
-  db.collection('documents').updateOne({field1: 0}, {$inc: {items: 1}}, function (err, product) {})
+  db.collection('documents').updateOne({ field1: 0 }, {$inc: { items: 1 }})
   res.send('ok')
 })
 
